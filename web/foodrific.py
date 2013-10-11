9335add4590b4604253f779464450e1ac4177452
André Freitas <p.andrefreitas@gmail.com>
2013-10-11 10:25:47 +0100

    Added hello world on the web app

---
+++ web/foodrific.py
@@ -0,0 +1,12 @@
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Foodrific - Soon!')
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
