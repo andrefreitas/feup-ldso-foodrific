442ed19e2a9f526b1735293b4eb777ea8d8264c4
Ana Gomes <acgg.gomes@gmail.com>
2013-10-11 17:32:32 +0100

    Classes's definition completed.

--- web/model/admin.py
+++ web/model/admin.py
@@ -2,6 +2,6 @@ from google.appengine.ext import ndb
 
 #Class admin
 class Admin(ndb.Model):
-    name = ndb.UserProperty()
-    email = ndb.StringProperty(indexed=False)
-    password = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    password = ndb.StringProperty(indexed=False)
