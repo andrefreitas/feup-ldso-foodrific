c1614bd390137626cd0b2cf16a9b95e29911edfd
Ana Gomes <acgg.gomes@gmail.com>
2013-10-12 19:44:52 +0100

    Added put to database.

--- web/model/data.py
+++ web/model/data.py
@@ -4,6 +4,7 @@ from google.appengine.ext import ndb
 def addUser(name, email, password, gender, birthday):
 	encrypted_pw = encrypt(password)
 	user = User(name=name, email=email, password=encrypted_pw, gender=gender, birthday=birthday)
+	user.put()
 
 def isUser(email):
 	user_query = ndb.GqlQuery("SELECT email FROM User " + " WHERE email = :1", email)
@@ -15,9 +16,11 @@ def isUser(email):
 
 def addPost(user, title, photo, rating):
 	post = Post(user=user, title=title, photo=photo, rating=rating)
+	post.put()
 
 def addYummy(user, post):
 	yummy = Yummy(user=user, post=post)
+	yummy.put()
 
 # Get crypt algorithm to use
 def encrypt(text):
