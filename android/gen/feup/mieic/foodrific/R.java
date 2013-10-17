8f947e9efa3ce7bd6957dd61a343f79327c29f26
ruiG <rui.nf1@gmail.com>
2013-10-17 14:01:15 +0100

    More buttons!

--- android/gen/feup/mieic/foodrific/R.java
+++ android/gen/feup/mieic/foodrific/R.java
@@ -24,10 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int LgWGoogleButton=0x7f080004;
         public static final int LoginButton=0x7f080002;
         public static final int PasswordEditText=0x7f080001;
+        public static final int RegisterButton=0x7f080003;
         public static final int UsernameEditText=0x7f080000;
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -36,7 +38,10 @@ public final class R {
         public static final int login=0x7f070000;
     }
     public static final class string {
+        public static final int LoginButton=0x7f050004;
+        public static final int LoginWithGoogle=0x7f050006;
         public static final int PasswordHint=0x7f050003;
+        public static final int RegisterButton=0x7f050005;
         public static final int UsernameHint=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
