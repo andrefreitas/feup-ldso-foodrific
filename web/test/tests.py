b2ba5e6c6d656d92d21866ac9689526b52bc638e
Ana Gomes <acgg.gomes@gmail.com>
2013-10-22 23:40:34 +0100

    Finished adding unit tests of user. Still need to improve search by name.

--- web/test/tests.py
+++ web/test/tests.py
@@ -24,15 +24,21 @@ class TestUser(DataStoreTestCase, unittest.TestCase):
         self.assertEqual(User.all().count(), 1)
         
     def test_datastore_login(self):
+        self.assertEqual(User.all().count(), 0)
         addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
         self.assertNotEqual(loginUser("susana@gmail.com", "iauhd83ISH"), False)
         
     def test_datastore_is_user(self):
+        self.assertEqual(User.all().count(), 0)
         addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
         self.assertTrue(isUser("susana@gmail.com"))
         
     def test_datastore_search_user_email(self):
+        self.assertEqual(User.all().count(), 0)
         addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
         user = searchUserByEmail("susana@gmail.com")
         self.assertTrue(user.email=="susana@gmail.com")
         
@@ -45,9 +51,20 @@ class TestUser(DataStoreTestCase, unittest.TestCase):
         for user in users:
             self.assertEqual(user.name, "Susana")"""
         
-    def test_datastore_edit_user(self):
+    def test_datastore_delete_user(self):
+        self.assertEqual(User.all().count(), 0)
         addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
+        user = searchUserByEmail("susana@gmail.com")
+        deleteUser(user.key().id())
+        self.assertEqual(User.all().count(), 0)
+        
+    def test_datastore_edit_user(self):
+        self.assertEqual(User.all().count(), 0)
+        addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
         editUser("SusanaG", "susana@gmail.com", "susanag@gmail.com", "123456", "f", date(1980, 3, 20))
+        self.assertEqual(User.all().count(), 1)
         user = searchUserByEmail("susanag@gmail.com")
         self.assertEqual(user.name, "SusanaG")
         self.assertEqual(user.email, "susanag@gmail.com")
@@ -56,7 +73,9 @@ class TestUser(DataStoreTestCase, unittest.TestCase):
         self.assertEqual(user.birthday, date(1980, 3, 20))
         
     def test_datastore_search_user_id(self):
+        self.assertEqual(User.all().count(), 0)
         addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
         user = searchUserByEmail("susana@gmail.com")
         user2 = searchUserByID(user.key().id()) 
         self.assertEqual(user.name, user2.name)
@@ -65,8 +84,22 @@ class TestUser(DataStoreTestCase, unittest.TestCase):
         self.assertEqual(user.gender, user2.gender)
         self.assertEqual(user.birthday, user2.birthday)
         
-       
         
+    def test_datastore_photo(self):
+        self.assertEqual(User.all().count(), 0)
+        addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
+        self.assertEqual(User.all().count(), 1)
+        user = searchUserByEmail("susana@gmail.com")
+        user_id = user.key().id()
+        addPhotoToUser(user_id, "photo_test")
+        photo = getPhotoByUser(user_id)
+        self.assertEqual(photo, "photo_test")
+        editPhotoUser(user_id, "another_photo_test")
+        photo_edit = getPhotoByUser(user_id)
+        self.assertEqual(photo_edit, "another_photo_test")
         
+    
+        
+    
 
         
