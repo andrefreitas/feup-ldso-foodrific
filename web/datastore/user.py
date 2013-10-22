438cfdbf1b532e95d7e8a7b09d0ad94b708d3c14
Ana Gomes <acgg.gomes@gmail.com>
2013-10-22 20:32:44 +0100

    Added more unitary tests.

--- web/datastore/user.py
+++ web/datastore/user.py
@@ -5,7 +5,7 @@ import base64
 class User(db.Model):
     name = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
-    password = db.StringProperty(indexed=False, required=True)
+    password = db.StringProperty(required=True)
     birthday = db.DateProperty(required=True)
     gender = db.StringProperty(indexed=False, required=True, choices=set(["m", "f"]))
     photo = db.BlobProperty()
