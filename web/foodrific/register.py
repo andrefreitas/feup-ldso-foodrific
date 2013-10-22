1e4952f4aec5bc5395452eb4dd14a23ef424533c
António Garcez <antonio_ribeiro01@hotmail.com>
2013-10-22 16:58:21 +0100

    Criação de ficheiros para o registo/login dos utilizadores

---
+++ web/foodrific/register.py
@@ -0,0 +1,26 @@
+import webapp2
+import cgi
+import datetime
+
+class RegisterPage(webapp2.RequestHandler):
+    
+    def post(self):
+        # Obter dados do formulario
+        name = cgi.escape(self.request.get('name'))
+        email = cgi.escape(self.request.get('email'))
+        password = cgi.escape(self.request.get('password'))
+        gender = cgi.escape(self.request.get('gender'))
+        day_birthday = cgi.escape(self.request.get('day'))
+        month_birthday = cgi.escape(self.request.get('month'))
+        year_birthday = cgi.escape(self.request.get('year'))
+        
+        # Construir uma data valida para ser inserida na base de dados
+        birthday_date = datetime.date(int(year_birthday), int(month_birthday), int(day_birthday))
+        
+        self.response.write(name);
+        self.response.write(email);
+        self.response.write(password);
+        self.response.write(gender);
+        self.response.write(birthday_date);
+        
+        
--- web/foodrific/sign.py
+++ web/foodrific/sign.py
@@ -1,11 +1,11 @@
 import webapp2
-import jinja2
-import os
 import cgi
 
 class LoginPage(webapp2.RequestHandler):
-
+    
     def post(self):
-        self.response.write(cgi.escape(self.request.get('email')))
-        self.response.write(cgi.escape(self.request.get('password')))
-
+        email = cgi.escape(self.request.get('email'))
+        password = cgi.escape(self.request.get('password'))
+        
+        self.response.write(email)
+        self.response.write(password)
