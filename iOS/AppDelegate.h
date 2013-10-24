48bf392f028151839f204c71bbfe46a7e53fdd2c
Nelson Mendes <nelson.daniel.mendes@gmail.com>
2013-10-24 16:40:07 +0100

    novamente adicionada Ios app

---
+++ iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  foodrific
+//
+//  Created by Nelson Mendes on 10/24/13.
+//  Copyright (c) 2013 Nelson Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
