a0fa964d3d8b142b28fea64c1efaedc4b0fdc0ef
Joaquim Barros <jruirbarros@gmail.com>
2013-11-05 21:45:46 +0000

    Added key to protect cleanTokens

--- web/tasks/clean_tokens.py
+++ web/tasks/clean_tokens.py
@@ -2,11 +2,15 @@ import webapp2
 from google.appengine.ext import db
 from datastore import user
 
+KEYDEF = "TreyYWtmtDM33VbontpHbQJ63eGwByfz6N1INuaJeJ0ZQ7qxWs3AdB0o5bLlGOFc"
+
 class CleanTokens(webapp2.RequestHandler):
     
     def get(self):
-        user_query = db.GqlQuery("SELECT * FROM User")
-        user_verify = user_query.fetch(1000)
-        for user in user_verify:
-            user.token = ""
-            db.put(user_verify)
+        key = self.request.get("key")
+        if(key == KEYDEF):
+            user_query = db.GqlQuery("SELECT * FROM User")
+            user_verify = user_query.fetch(1000)
+            for user in user_verify:
+                user.token = ""
+                db.put(user_verify)
