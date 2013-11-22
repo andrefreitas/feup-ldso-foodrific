c2b69a4bb7862fe5a583ef545215c78a857d8d7d
Ana Gomes <acgg.gomes@gmail.com>
2013-11-22 22:52:33 +0000

    Added unit tests to send recover.

--- web/test/testApiSendRecover.py
+++ web/test/testApiSendRecover.py
@@ -8,9 +8,9 @@ from gaetestbed import DataStoreTestCase
 from datastore.user import addUser
 from datetime import date
 
-class testApiDeletePost(DataStoreTestCase, unittest.TestCase):
+class testApiSendRecover(DataStoreTestCase, unittest.TestCase):
     def setUp(self):
-        app = webapp2.WSGIApplication([('/api/register_verification', api.RegisterVerification)])
+        app = webapp2.WSGIApplication([('/api/send_recover', api.SendRecover)])
         self.testapp = webtest.TestApp(app)
         self.testbed = testbed.Testbed()
         self.testbed.activate()
@@ -20,13 +20,13 @@ class testApiDeletePost(DataStoreTestCase, unittest.TestCase):
         
     def testEmailNonExistent(self):
         self.testbed.init_memcache_stub()
-        response = self.testapp.get('/api/register_verification?email=peter@gmail.com')
+        response = self.testapp.get('/api/send_recover?email=peter@gmail.com')
         self.assertEqual(response.status, "200 OK")
-        self.assertEqual(response.json, {'answer':'valid', 'result': 'ok'})
+        self.assertEqual(response.json, {'answer':'invalid', 'result': 'ok'})
         
     def testEmail(self):
         addUser("Susana", "susana@gmail.com", "iauhd83ISH", "f", date(1985, 5, 22))
         self.testbed.init_memcache_stub()
-        response = self.testapp.get('/api/register_verification?email=susana@gmail.com')
+        response = self.testapp.get('/api/send_recover?email=susana@gmail.com')
         self.assertEqual(response.status, "200 OK")
-        self.assertEqual(response.json, {'answer':'invalid', 'result': 'ok'})
+        self.assertEqual(response.json, {'answer':'valid', 'result': 'ok'})
