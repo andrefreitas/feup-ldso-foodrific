d8da444f9e35d4f85608d3dca99aafb6dce34b72
Ana Gomes <acgg.gomes@gmail.com>
2013-11-22 10:56:48 +0000

    Solved api of deletePost exception.

--- web/api/delete_post.py
+++ web/api/delete_post.py
@@ -1,6 +1,10 @@
 import webapp2
-from datastore import *
+from datastore.user import *
+from datastore.comment import *
+from datastore.yummy import *
+from datastore.post import *
 import json
+from datastore.post import getPostByID
 
 class DeletePost(webapp2.RequestHandler):
 
@@ -10,14 +14,15 @@ class DeletePost(webapp2.RequestHandler):
 			id_post_str = self.request.get("postId")
 			id_post = int(id_post_str)
 			self.response.headers['Content-Type'] = 'application/json'
-			if(deletePost(id_post)):
-				output["answer"] = "valid"
+			if (getPostByID(id_post)):
 				deleteCommentsForPost(id_post)
 				deletePostYummys(id_post)
-			else:
-				output["answer"] = "invalid"
-			output["result"] = "ok"
-			self.response.out.write(json.dumps(output))
+				if(deletePost(id_post)):
+					output["answer"] = "valid"
+				else:
+					output["answer"] = "invalid"
+				output["result"] = "ok"
+				self.response.out.write(json.dumps(output))
 		except:
 			output["result"] = "error"
 			self.response.out.write(json.dumps(output))
