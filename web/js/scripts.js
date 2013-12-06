3ad2297f595207d14cc2ac17cc268a879f195384
Ana Gomes <acgg.gomes@gmail.com>
2013-12-06 21:26:02 +0000

    Adicionado datepicker para a data de nascimento.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -4,11 +4,26 @@ window.comment_id;
 window.post_id;
 
 $(document).ready(function(){
-	
 	$( "#datepicker" ).datepicker({
-		showOn: "button",
-		buttonImage: "images/calendar.gif",
-		buttonImageOnly: true
+		showButtonPanel: true,
+		changeMonth: true,
+		changeYear: true,
+		yearRange:'-90:-12',
+		defaultDate: '-12Y',
+		reverseYearRange: true,
+		closeText: 'Fechar',
+		prevText: '&#x3c;Anterior',
+		nextText: 'Seguinte',
+		currentText: 'Hoje',
+		monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho',
+		'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+		monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun',
+		'Jul','Ago','Set','Out','Nov','Dez'],
+		dayNames: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','S&aacute;bado'],
+		dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','S&aacute;b'],
+		dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','S&aacute;b'],
+		weekHeader: 'Sem',
+		dateFormat: 'dd/mm/yy'
 	});
 	
 	$('#register').click(function(){
