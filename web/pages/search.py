74537c386d93cc9e1de52eafc6b51ed4b275af24
Ana Gomes <acgg.gomes@gmail.com>
2013-12-11 16:08:17 +0000

    A verificar se tem utilizador com login feito.

--- web/pages/search.py
+++ web/pages/search.py
@@ -15,34 +15,33 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class SearchResults(BaseHandler):
 
     def get(self):
-        term = self.request.get("term")
-        output = {}
-        posts = searchPosts(term)
-        user_id = int(self.get_session_user_id())
-        # Get post yummis
-        def putYummys(post):
-            post_user_id = post.user.key().id()
-            post_id = post.key().id()
-            yummys = getPostYummys(post_id)
-            post.yummys = len(yummys)
-            post.yummyDone = YummyDone(user_id, post_id)
-            return post
-
-        # Get post comments
-        def putComments(post):
-            post_id = post.key().id()
-            comments = getCommentsForPost(post_id)
-            post.comments = sorted(comments, key=lambda c: c.date, reverse=True)
-            post.commentsNumber = len(comments)
-            return post
-
-        
-
-        posts = map(putYummys, posts)
-        posts = map(putComments, posts)
-        template_values = {
-            "term" : term,
-            "posts": posts
-        }
-        template = JINJA_ENVIRONMENT.get_template('search.html')
-        self.response.write(template.render(template_values))
+        if(self.isLoggedIn()):
+            term = self.request.get("term")
+            posts = searchPosts(term)
+            user_id = int(self.get_session_user_id())
+            # Get post yummis
+            def putYummys(post):
+                post_id = post.key().id()
+                yummys = getPostYummys(post_id)
+                post.yummys = len(yummys)
+                post.yummyDone = YummyDone(user_id, post_id)
+                return post
+    
+            # Get post comments
+            def putComments(post):
+                post_id = post.key().id()
+                comments = getCommentsForPost(post_id)
+                post.comments = sorted(comments, key=lambda c: c.date, reverse=True)
+                post.commentsNumber = len(comments)
+                return post
+    
+            posts = map(putYummys, posts)
+            posts = map(putComments, posts)
+            template_values = {
+                "term" : term,
+                "posts": posts
+            }
+            template = JINJA_ENVIRONMENT.get_template('search.html')
+            self.response.write(template.render(template_values))
+        else:
+            return self.redirect('/')
