ae7103adaf185a50fa0f8f9a5a1f51f8df88d236
André Freitas <p.andrefreitas@gmail.com>
2013-12-17 12:31:14 +0000

    No feed aparece agora os posts do próprio autor.

--- web/pages/feed.py
+++ web/pages/feed.py
@@ -17,6 +17,7 @@ class Feed(BaseHandler):
         if(self.isLoggedIn()):
             user_id = int(self.get_session_user_id())
             posts = getPostsByUserFollowing(user_id)
+            posts = posts + getPostsByUser(user_id)
 
             posts = sorted(posts, key = lambda post: post.original_date, reverse=True)
 
